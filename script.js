a4563a992ac7106784eb5fb348ebf42bf5150e2a
Marah Salah <144604801+MarahSalah@users.noreply.github.com>
2023-10-11 15:07:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBd04IDbFSNFWaaOB3hS5OUi7VTbsq2uSs",
-//     authDomain: "login-a2bae.firebaseapp.com",
-//     databaseURL: "https://login-a2bae-default-rtdb.firebaseio.com",
-//     projectId: "login-a2bae",
-//     storageBucket: "login-a2bae.appspot.com",
-//     messagingSenderId: "390282070895",
-//     appId: "1:390282070895:web:22830863bf2e760e97369a",
-//     measurementId: "G-9YV9LT752J"
-//   };
